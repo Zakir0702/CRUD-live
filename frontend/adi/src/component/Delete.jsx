79c0d75afca9465ca380589cb9d96c3eab4905f5
Zakir0702 <officialzak0702@gmail.com>
2025-05-01 15:33:26 +0530

    frontend updated

--- frontend/adi/src/component/Delete.jsx
+++ frontend/adi/src/component/Delete.jsx
@@ -1,22 +1,22 @@
-import axios from 'axios';
-import React from 'react'
+import axios from "axios";
+import React from "react";
 
 function Delete() {
-    const adi=async(e)=>{
-        e.preventDefault
-        const id=e.target.id.value;
-        await axios.delete(`http://localhost:9000/users/${id}`)
-        alert("data delete")
-    }
+  const adi = async (e) => {
+    e.preventDefault;
+    const id = e.target.id.value;
+    await axios.delete(`https://crud-live-1-pem3.onrender.com/users/${id}`);
+    alert("data delete");
+  };
   return (
-    <div style={{border:'2px solid red', padding:'10px'}}>
-        <h1>Delete Data</h1>
-        <form onSubmit={adi}>
-            <input type="text" name="id" placeholder='enter id' />
-            <button type='submit'>Delete Data</button>
-        </form>
+    <div style={{ border: "2px solid red", padding: "10px" }}>
+      <h1>Delete Data</h1>
+      <form onSubmit={(e) => adi(e)}>
+        <input type="text" name="id" placeholder="enter id" />
+        <button type="submit">Delete Data</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Delete
+export default Delete;
--- frontend/adi/src/component/Register.jsx
+++ frontend/adi/src/component/Register.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 const Register = () => {
-    const handleregister =async (e) =>{
-        e.preventDefault()
-        const user = {
-            name: e.target.name.value,
-            age: e.target.age.value
-        }
-        await axios.post('http://localhost:9000/users', user)
-       alert('User Registered Successfully')
-
-    }
+  const handleregister = async (e) => {
+    e.preventDefault();
+    const user = {
+      name: e.target.name.value,
+      age: e.target.age.value,
+    };
+    await axios.post("https://crud-live-1-pem3.onrender.com/users", user);
+    alert("User Registered Successfully");
+  };
   return (
-    <div style={{border:'2px solid red', padding:'10px'}}>
-      <h1 style={{color:'red'}}>Resister User</h1>
+    <div style={{ border: "2px solid red", padding: "10px" }}>
+      <h1 style={{ color: "red" }}>Resister User</h1>
       <form onSubmit={handleregister}>
-        <label>Name:</label> <input type='text' name='name' />
-        <label>Age:</label> <input type='text' name='age' />
-        <button type='submit'>Register</button>
+        <label>Name:</label> <input type="text" name="name" />
+        <label>Age:</label> <input type="text" name="age" />
+        <button type="submit">Register</button>
       </form>
     </div>
-  )
-}
-export default Register
+  );
+};
+export default Register;
