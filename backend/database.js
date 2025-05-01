58c92a853e717f803e72f73b4817dc12193e697a
Zakir0702 <officialzak0702@gmail.com>
2025-05-01 14:45:12 +0530

    name add

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,12 @@
-const mongoose =require('mongoose');
-async function dbConn(){
-    const conn=await mongoose.connect('mongodb+srv://zakir0702:zakir0702@cluster0.s8jd0e4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-    if(conn){
-        console.log('database connect successfully')
-
-    }
-    else{
-        console.log('connection fail');
-    }
+const mongoose = require("mongoose");
+async function dbConn() {
+  const conn = await mongoose.connect(
+    "mongodb+srv://zakir0702:zakir0702@cluster0.s8jd0e4.mongodb.net/crud-live-cse-c?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  if (conn) {
+    console.log("database connect successfully");
+  } else {
+    console.log("connection fail");
+  }
 }
-module.exports=dbConn;
+module.exports = dbConn;
